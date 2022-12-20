aeb23b2e5dbf46d26946d567f6a02310cf0493fc
Redondo <David.Redondo@avl.com>
2022-12-20 12:41:23 +0100

    inicio

---
+++ Scipy_code.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+@author: David Redondo Quintero
+Github: Drq13112
+"""
+"""
+Scipy is a scientific computacion library which uses numpy underneath.
+The main reason why we don't use numpy to do the task that we do with numpy, 
+is because it provides few more utilities for optimisation, stats and signal procesing.
+"""
+
+from scipy import constants
+
+print(constants.liter)
